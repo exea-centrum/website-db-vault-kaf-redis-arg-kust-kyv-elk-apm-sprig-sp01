a2ddfcc841e1b80e80fa8f6a14dab22584b8556d
Dawid Trojanowski <exea.centrum@gmail.com>
2026-09-03 09:39:18 +0200

    fix: type spark foreach batch callback

--- spark-jobs/src/main/scala/com/davtro/jobs/MarketingAnalyticsJob.scala
+++ spark-jobs/src/main/scala/com/davtro/jobs/MarketingAnalyticsJob.scala
@@ -10,7 +10,7 @@ object MarketingAnalyticsJob {
     val parsedDF = kafkaDF.selectExpr("CAST(value AS STRING) as json").select(from_json($"json", new org.apache.spark.sql.types.StructType().add("event", "string").add("property_id", "integer").add("guest_email", "string").add("booking_value", "double").add("timestamp", "string")).as("data")).select("data.*")
     val aggDF = parsedDF.withWatermark("timestamp", "10 minutes").groupBy(window($"timestamp", "5 minutes"), $"property_id").agg(count("*").as("booking_count"), sum("booking_value").as("total_revenue"), avg("booking_value").as("avg_booking_value"))
     val query = aggDF.writeStream.outputMode("update").format("console").trigger(Trigger.ProcessingTime("10 seconds")).start()
-    val jdbcDF = parsedDF.writeStream.foreachBatch { (batchDF, batchId) => batchDF.write.format("jdbc").option("url", "jdbc:postgresql://postgres-db:5432/davtro_rentals").option("dbtable", "marketing_events").option("user", "davtro").option("password", "changeme").mode("append").save() }.start()
+    val jdbcDF = parsedDF.writeStream.foreachBatch { (batchDF: org.apache.spark.sql.Dataset[org.apache.spark.sql.Row], batchId: Long) => batchDF.write.format("jdbc").option("url", "jdbc:postgresql://postgres-db:5432/davtro_rentals").option("dbtable", "marketing_events").option("user", "davtro").option("password", "changeme").mode("append").save() }.start()
     query.awaitTermination(); jdbcDF.awaitTermination()
   }
 }
